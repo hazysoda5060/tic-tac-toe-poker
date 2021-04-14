394a5f110226ed2d6d34188ef375c2e3ad96b0dc
alex <alexanderstapp@gmail.com>
2021-04-14 16:53:49 -0600

    close to finished with tic tac toe

--- Part1/index.js
+++ Part1/index.js
@@ -8,38 +8,59 @@ function play(clickedID){
     const playerSpan = document.getElementById('player');
     const clickedElement = document.getElementById(clickedID);
 
-    if (playerSpan.innerText === 'X'){
+    if(clickedElement.innerText === 'X' || clickedElement.innerText === 'O'){
+        alert('cheater')
+    } else if(playerSpan.innerText === 'X'){
         playerSpan.innerText = 'O'
         clickedElement.innerText = 'X'
         board[clickedID] = 'X'
+        victoryCheck()
     } else {
         playerSpan.innerText = 'X'
         clickedElement.innerText = 'O'
         board[clickedID] = 'O'
+        victoryCheck()
+    }
+
+}
+
+
+function victoryCheck(){
+    // horizontal victory
+    for( i = 0 ; i < 9 ; i++ ){
+        if(board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
+            alert(`${board[i]} is the winner!`)
+            victoryReset()
+        }
+    }
+    
+    // vertical victory
+    for( i = 0 ; i < 9 ; i++ ){
+        if(board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
+            alert(`${board[i]} is the winner!`)
+            victoryReset()
+        }
+    }
+    
+    // diagonal victories
+    if(board[0] === board[4] && board[4] === board[8]) {
+        alert(`${board[4]} is the winner!`)
+        victoryReset()
+    }
+    
+    if(board[2] === board[4] && board[4] === board[6]) {
+        alert(`${board[4]} is the winner!`)
+        victoryReset()
+    }
+}
+
+function victoryReset(){
+    for( i = 0 ; i < 9 ; i++ ){
+        boxReset = document.getElementById(i)
+        boxReset.innerText = ''
+    }
+    for(i = 0 ; i < 9 ; i++){
+        board.splice(i, 1, i)
     }
     console.log(board)
-}
-
-// horizontal victory
-for( i = 0 ; i < 9 ; i++ ){
-    if(board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
-     alert(`${board[i]} is the winner!`)
-    }
-}
-
-// vertical victory
-for( i = 0 ; i < 9 ; i += 3 ){
-    if(board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
-     alert(`${board[i]} is the winner!`)
-    }
-}
-
-// diagonal victories
-
-if(board[0] === board[4] && board[4] === board[8]) {
-    alert(`${board[i]} is the winner!`)
-}
-
-if(board[2] === board[4] && board[4] === board[6]) {
-    alert(`${board[i]} is the winner!`)
 }
